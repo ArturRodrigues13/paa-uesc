98e7a24d72f4446702a3489e44c0c1d95624bf87
Artur Rodrigues Santos <arturrodriguesfla13@gmail.com>
2025-08-28 08:35:45 -0300

    insertion_sort

---
+++ Exercicio1/insertion_sort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void insertion_sort(int* vet, int tamanho) {
+
+	for(int i = 1; i < tamanho; i++) {
+
+		int chave = vet[i];
+		int j = i - 1;
+		while (j >= 0 && vet[j] > chave)
+		{
+			vet[j + 1] = vet[j];
+			j--;
+		}
+		vet[j + 1] = chave;
+	}
+}
+
+int main() {
+
+	int tamanho;
+
+	scanf("%d",&tamanho);
+
+	int *vet = (int*)malloc(sizeof(int)*tamanho);
+
+	for (int i = 0; i < tamanho;i++)
+	{
+		scanf("%d",&vet[i]);
+	}
+
+	insertion_sort(vet,tamanho);
+
+	for (int i = 0; i < tamanho;i++)
+	{
+		printf("%d ",vet[i]);
+	}
+
+	return 0;
+}
