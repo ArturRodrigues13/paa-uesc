0668d44e1e6e26f000954d7c2219f61244d4b71b
Artur Rodrigues Santos <arturrodriguesfla13@gmail.com>
2025-08-28 23:59:36 -0300

    Finaliza (vou sofrer tanto...)

--- Exercicio1/shake_sort.c
+++ Exercicio1/shake_sort.c
@@ -32,9 +32,9 @@ void shake_sort(int* vet, int tamanho) {
 	int inicio = 0;
 	int final = tamanho - 1;
 
-	while(inicio != final) {
+	while(inicio != final) { // (N - 1) + (N - 2) + (N - 3) + (N - 4) + (N - 5) + (N - 6) + ... + 1
 
-		for(int i = inicio; i < final; i++) {
+		for(int i = inicio; i < final; i++) { // (N - 1) + (N - 3) + (N - 5) + (N - 7) + ... + 1
 			if(vet[i] > vet[i + 1]) {
 
 				int aux = vet[i];
@@ -42,12 +42,15 @@ void shake_sort(int* vet, int tamanho) {
 				vet[i + 1] = aux;
 			}
 		}
+
 		final --;
 
-		if(inicio == final) break;
+		if(inicio == final) {
 
-		for(int i = final; i > inicio; i--) {
+			break;
+		}
 
+		for(int i = final; i > inicio; i--) { // (N - 2) + (N - 4) + (N - 6) + (N - 8) + ... + 2
 			if(vet[i] < vet[i - 1]) {
 
 				int aux = vet[i];
